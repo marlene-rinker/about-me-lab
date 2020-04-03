1c3e1659107e0928ae3f8ef00c15f692933df5d6
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-04-02 17:46:25 -0700

    removed unnecessary comments

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
 'use strict';
 
-// Send the user a personalized welcome message
-// Ask the user their name using a prompt()
+//This is a script that asks a user questions related to me when they come to the site.
+
 var userName = prompt('Hi. What\'s your name?');
-
-// Display the name back to the user in a custom greeting
-// console.log('Welcome to my website, ' + userName + '. It\'s nice to meet you. Let\'s play a quick game.');
 alert('Welcome to my website, ' + userName + '. It\'s nice to meet you. Let\'s get to know each other.');
-
-
-// Ask a user five yes or no questions
-// The answer must be y/n or yes/no
-// Normalize the answer for case sensitivity
-// Let the user know they answered the question correctly
 
 // Set up a message to display if the answer isn't yes or no
 var wthMessage = 'Sorry, ' + userName + '. I don\'t know what that means. Let\'s move on.';
 
-// Set up counter variables for right and wrong answers
-var rightAnswer = 0;
-// declaring variable i
+
+var rightAnswer = 0;//counter variable to record right answers
 var i;
 
-// create functions
+// create functions to ask the questions
 function livesInSeattleQuestion(){
   // Ask the user if they live in Seattle
   var livesInSeattle = prompt('Do you live in Seattle?');
   console.log('Lives in Seattle:' +livesInSeattle);
 
   if (livesInSeattle.toLowerCase() === 'y' || livesInSeattle.toLowerCase() === 'yes'){
-    // console.log('Cool! I live in Seattle, too.');
     alert('Cool! I live in Seattle, too.');
     rightAnswer = rightAnswer + 1;
   } else if (livesInSeattle.toLowerCase() === 'n' || livesInSeattle.toLowerCase() === 'no'){
-    // console.log('Seattle is a fun place. You should visit sometime.');
     alert('Seattle is a fun place. You should visit sometime.');
   } else {
-    // console.log(wthMessage);
     alert(wthMessage);
   }
 }
-
 
 function vegetarianQuestion(){
   // Ask the user if they are a vegetarian
@@ -48,18 +34,14 @@ function vegetarianQuestion(){
   console.log('Vegetarian: ' + vegetarian);
 
   if (vegetarian.toLowerCase() === 'y' || vegetarian.toLowerCase() === 'yes'){
-    // console.log('Great choice. I\'m a vegetarian, too!');
     alert('Great choice. I\'m a vegetarian, too!');
     rightAnswer = rightAnswer + 1;
   }else if (vegetarian.toLowerCase() === 'n' || vegetarian.toLowerCase() === 'no'){
-    // console.log('If you want to try it, a good way to start is with Meatless Mondays!');
     alert('If you want to try it, a good way to start is with Meatless Mondays!');
   }else {
-    // console.log(wthMessage);
     alert(wthMessage);
   }
 }
-
 
 function hasDogQuestion(){
   // Ask the user if they have a dog
@@ -67,18 +49,14 @@ function hasDogQuestion(){
   console.log('Has a dog: ' + hasDog);
 
   if (hasDog.toLowerCase() === 'y' || hasDog.toLowerCase() === 'yes'){
-    // console.log('How fun! I love dogs.');
     alert('How fun! I love dogs.');
     rightAnswer = rightAnswer + 1;
   }else if (hasDog.toLowerCase() === 'n' || hasDog.toLowerCase() === 'no'){
-    // console.log('That\'s too bad. Dogs are great!');
     alert('That\'s too bad. Dogs are great!');
   }else {
-    // console.log(wthMessage);
     alert(wthMessage);
   }
 }
-
 
 function likesHikingQuestion(){
   // Ask the user if they like hiking
@@ -86,18 +64,14 @@ function likesHikingQuestion(){
   console.log('Likes hiking: ' + likesHiking);
 
   if (likesHiking.toUpperCase() === 'Y' || likesHiking.toUpperCase() === 'YES') {
-    // console.log('Me, too! There are so many pretty places to get out and explore.');
     alert('Me, too! There are so many pretty places to get out and explore.');
     rightAnswer = rightAnswer + 1;
   }else if (likesHiking.toUpperCase() === 'N' || likesHiking.toUpperCase() === 'NO'){
-    // console.log('That\'s ok. There are other fun ways to experience the outdoors.');
     alert('That\'s ok. There are other fun ways to experience the outdoors.');
   }else {
-    // console.log(wthMessage);
     alert(wthMessage);
   }
 }
-
 
 function likesReadingQuestion(){
   // Ask the user if they like reading
@@ -105,18 +79,14 @@ function likesReadingQuestion(){
   console.log('Likes reading: ' +likesReading);
 
   if (likesReading.toUpperCase() === 'Y' || likesReading.toUpperCase() === 'YES'){
-    // console.log('I like reading too. I\'d rather read than watch TV.');
     alert('I like reading too. I\'d rather read than watch TV.');
     rightAnswer = rightAnswer + 1;
   }else if (likesReading.toUpperCase() === 'N' || likesReading.toUpperCase() === 'NO'){
-    // console.log('Oh well. We don\'t have to like all of the same things.');
     alert('Oh well. We don\'t have to like all of the same things.');
   }else{
-    // console.log(wthMessage);
     alert(wthMessage);
   }
 }
-
 
 function numberGuessQuestion(){
   //generate a random number between 1 and 20 for the user to guess
@@ -153,24 +123,18 @@ function numberGuessQuestion(){
   }
 }
 
-
-// Make a guessing game
-// Multiple answers stored in an array
-// Six attempts to answer
-//Display all the possible correct answers to the user
-
+// Ask a guessing game question with multiple possible answers
 
 function myDogsQuestion(){
   var myDogs = ['Charlie', 'Dusty', 'Ginger', 'Scruffy', 'Max', 'Rocky', 'Phoebe', 'Orry'];
 
   var dogGuess = prompt('Although I don\'t have a dog right now, I have had some great ones in the past. Can you guess one of their names?');
-  
-  i = 0;//making sure i is set back to 0
+
+  i = 0;
   while (i < 5){
     var theyGuessedRight = false;
     for(var j = 0; j < myDogs.length; j++){
       if (myDogs[j].toLowerCase() === dogGuess.toLowerCase()){
-      // alert('Yes! I had a wonderful dog named ' + dogGuess + '.');
         i = 5;
         j = myDogs.length + 1;
         theyGuessedRight = true;
@@ -187,12 +151,12 @@ function myDogsQuestion(){
     i++;
   }
   alert('Here\'s a list of all my wonderful dogs: '+ myDogs);
-  
+
   console.log('right answers: ' + rightAnswer);
-  
+
 }
 
-// call the question functions
+// ask the questions
 livesInSeattleQuestion();
 vegetarianQuestion();
 hasDogQuestion();
@@ -209,7 +173,6 @@ if (rightAnswer > 4){
 }
 
 // Display the user's name in a final message
-// console.log('Hope you\'re having a great day, ' + userName + '. Thanks for stopping by!');
 alert('Hope you\'re having a great day, ' + userName + '. Thanks for stopping by!');
 
 
